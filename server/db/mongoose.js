ac99bc337587d7610d9072cea3e840bae5209cd5
Ciu <alexcm8686@yahoo.com>
2018-10-25 00:04:33 +0300

    mlab

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,21 @@
 const mongoose = require('mongoose');
 
-//mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://127.0.0.1:27017/TodoApp', {useNewUrlParser: true });
+const {mdburi} = require('./../../temp/sec.js')
 
+let uri = mdburi();
+
+//mongoose.Promise = global.Promise;
+mongoose.connect(uri, {useNewUrlParser: true }).then(() =>
+{
+    console.log(`Conected to mongodb://<dbuser>:<dbpassword>@ds243502.mlab.com:43502/todo-app`);
+}, (err) =>{
+    uri = 'mongodb://127.0.0.1:27017/TodoApp';
+    mongoose.connect(uri, {useNewUrlParser: true })
+    console.log(`Conected to ${uri}. \n Mlab error: ${err}`);
+}
+
+);
+//console.log(mdburi())
 module.exports = {mongoose};
+
+//./../server
