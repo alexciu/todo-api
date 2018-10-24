41a0a412188be9c7e8d2d64f2d1010aa18ed1ac8
Ciu <alexcm8686@yahoo.com>
2018-10-25 00:14:29 +0300

    env uri

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,14 +2,13 @@ const mongoose = require('mongoose');
 
 const {mdburi} = require('./../../temp/sec.js')
 
-let uri = mdburi();
+let uri = process.env.DATABASE_URI || 'mongodb://127.0.0.1:27017/TodoApp';
 
 //mongoose.Promise = global.Promise;
 mongoose.connect(uri, {useNewUrlParser: true }).then(() =>
 {
     console.log(`Conected to mongodb://<dbuser>:<dbpassword>@ds243502.mlab.com:43502/todo-app`);
 }, (err) =>{
-    uri = 'mongodb://127.0.0.1:27017/TodoApp';
     mongoose.connect(uri, {useNewUrlParser: true })
     console.log(`Conected to ${uri}. \n Mlab error: ${err}`);
 }
