9234b47b15e7fa7b6f3bfd593d07d448254c6723
Ciu <alexcm8686@yahoo.com>
2018-10-25 00:24:08 +0300

    fix

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,13 +7,12 @@ let uri = process.env.DATABASE_URI || 'mongodb://127.0.0.1:27017/TodoApp';
 //mongoose.Promise = global.Promise;
 mongoose.connect(uri, {useNewUrlParser: true }).then(() =>
 {
-    console.log(`Conected to mongodb://<dbuser>:<dbpassword>@ds243502.mlab.com:43502/todo-app`);
+    console.log(`Conected to ${uri}`);
 }, (err) =>{
-    mongoose.connect(uri, {useNewUrlParser: true })
-    console.log(`Conected to ${uri}. \n Mlab error: ${err}`);
-}
+        console.log(`Unable to connect.`);
+}).catch((e) => {
 
-);
+});
 //console.log(mdburi())
 module.exports = {mongoose};
 
