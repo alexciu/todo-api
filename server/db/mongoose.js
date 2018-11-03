c32509c6b96cdb2ad27ba61d2512aa530653fbcf
Alex Ciu <alexcm8686@yahoo.com>
2018-11-03 16:51:14 +0200

    mint

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose');
 
-//const {mdburi} = require('./../../temp/sec.js')
-
 let uri = process.env.DATABASE_URI || 'mongodb://127.0.0.1:27017/TodoApp';
 
 //mongoose.Promise = global.Promise;
